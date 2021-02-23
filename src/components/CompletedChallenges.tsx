9f612b9b8d6a840fc7188f9c787cb7d9d156fab0
Ivo Vieira <schmelman.b@gmail.com>
2021-02-23 19:10:50 -0400

    ops

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/components/CompletedChallenges.module.css'
+
+export function CompletedChallenges() {
+  return (
+    <div className={styles.completedChallenges}>
+      <span>Desafios Completos</span>
+      <span>5</span>
+    </div>
+  )
+}
